968a5b100df181adeedf1acc826904e519023cd4
Nethan P <96836564+nethanchowdary@users.noreply.github.com>
2022-04-27 22:38:51 +0530

    Delete springjwt directory

--- springjwt/src/main/java/com/example/springjwt/repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.springjwt.repository;
-
-import com.example.springjwt.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<Users, Integer> {
-    Users findByUsername(String username);
-}
