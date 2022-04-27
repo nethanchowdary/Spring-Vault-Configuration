1500c07986ddf37b1bd1c4a368684a97f501281b
nethanchowdary <nethanchowdary@gmail.com>
2022-04-27 22:16:47 +0530

    spring-aws upload and download

---
+++ awsspring/src/main/java/com/example/awsspring/AwsspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsspringApplication.class, args);
+	}
+
+}
---
+++ awsspring/src/test/java/com/example/awsspring/AwsspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
